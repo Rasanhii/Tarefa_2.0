2f80b386eb261d9b8b5b2de9f7f2b1adb4f3b611
Rasanhii <107484176+Rasanhii@users.noreply.github.com>
2023-03-14 21:49:26 +0000

    terminado

--- ex_06.py
+++ ex_06.py
@@ -1,12 +1,9 @@
-capital_inicial = float(input("Digite o valor inicial da poupança: "))
+capi_ini = float(input("Digite o valor inicial da poupança: "))
 
-# Define a taxa de juros aplicada
-taxa_juros = 0.005
+juros = 0.005
 
-# Define o número de períodos (meses) em que o dinheiro ficará investido
 num_periodos = 12
 
-# Calcula e imprime o montante mês a mês durante um período de 12 meses
 for mes in range(1, num_periodos+1):
-    montante = capital_inicial * (1 + taxa_juros)**mes
+    montante = capi_ini * (1 + juros)**mes
     print(f"Montante no mês {mes}: R${montante:.2f}")
